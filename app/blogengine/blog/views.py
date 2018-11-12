fe522972ca132cd939ae5c73ba96fc909549d602
Vladyslav <fareomg@gmail.com>
2018-11-12 18:43:06 +0200

    Added base template

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def posts_list(request):
+    n = ['Vladik', 'Oladik']
+    return render(request, 'blog/index.html', context={'names': n})
