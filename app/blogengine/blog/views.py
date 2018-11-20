72687010474f7e8b105d89c83f923d1f2a6ef58a
Vladyslav <fareomg@gmail.com>
2018-11-21 00:19:56 +0200

    Created Post model, upgraded to py 3.7

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def posts_list(request):
-    n = ['Vladik', 'Oladik']
-    return render(request, 'blog/index.html', context={'names': n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
