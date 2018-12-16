3e595d05b7a48347d175d994bcf2e85ee860b941
Vladyslav <fareomg@gmail.com>
2018-12-16 13:05:40 +0200

    Created Tag model

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Tag
 
 # Create your views here.
 def posts_list(request):
@@ -10,3 +10,11 @@ def posts_list(request):
 def post_detail(request, slug):
     post = Post.objects.get(slug__iexact=slug)
     return render(request, 'blog/post_detail.html', context={'post': post})
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(request, 'blog/tag_detail.html', context={'tag': tag})
