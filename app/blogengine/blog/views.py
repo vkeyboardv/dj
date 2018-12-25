7b891d80be0e8465721e06a9e8a925e43458b165
Vladyslav <fareomg@gmail.com>
2018-12-25 19:57:47 +0200

    Created post_create form, ObjectCreateMixin

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import View
 
 from .models import Post, Tag
-from .utils import ObjectDetailMixin
-from .forms import TagForm
+from .utils import ObjectDetailMixin, ObjectCreateMixin
+from .forms import TagForm, PostForm
 
-# Create your views here.
+
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
@@ -20,6 +20,24 @@ class PostDetail(ObjectDetailMixin, View):
     # def get(self, request, slug):
     #     post = get_object_or_404(Post, slug__iexact=slug)
     #     return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+class PostCreate(ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'blog/post_create_form.html'
+
+    # def get(self, request):
+    #     form = PostForm()
+    #     return render(request, 'blog/post_create_form.html', context={'form': form})
+    #
+    #
+    # def post(self, request):
+    #     bound_form = PostForm(request.POST)
+    #
+    #     if bound_form.is_valid():
+    #         new_post = bound_form.save()
+    #         return redirect(new_post)
+    #     return render(request, 'blog/post_create_form.html', context={'form': bound_form})
 
 
 def tags_list(request):
@@ -36,16 +54,18 @@ class TagDetail(ObjectDetailMixin, View):
     #     return render(request, 'blog/tag_detail.html', context={'tag': tag})
 
 
-class TagCreate(View):
-    def get(self, request):
-        form = TagForm()
-        return render(request, 'blog/tag_create.html', context={'form': form})
+class TagCreate(ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'blog/tag_create.html'
 
-
-    def post(self, request):
-        bound_form = TagForm(request.POST)
-
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        return render(request, 'blog/tag_create.html', context={'form': bound_form})
+    # def get(self, request):
+    #     form = TagForm()
+    #     return render(request, 'blog/tag_create.html', context={'form': form})
+    #
+    # def post(self, request):
+    #     bound_form = TagForm(request.POST)
+    #
+    #     if bound_form.is_valid():
+    #         new_tag = bound_form.save()
+    #         return redirect(new_tag)
+    #     return render(request, 'blog/tag_create.html', context={'form': bound_form})
