4e441abd714dafb2fea4a8fe585d0d1f29f6d25b
Vladyslav <fareomg@gmail.com>
2019-01-13 22:32:21 +0200

    Activated search label

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -8,12 +8,22 @@ from .forms import TagForm, PostForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 
+from django.db.models import Q
+
 
 def posts_list(request):
-    posts = Post.objects.all()
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
     paginator = Paginator(posts, 2)
+
     page_number = request.GET.get('page', 1)
     page = paginator.get_page(page_number)
+
     is_paginated = page.has_other_pages()
 
     if page.has_previous():
